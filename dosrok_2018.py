a44387827a1507ebfce760f53ee62da5e942ce5b
BuddyLetsGoStudy <79104526527@yandex.ru>
2018-05-22 15:49:26 +0300

    27ое досрока добавлено

---
+++ dosrok_2018.py
@@ -0,0 +1,17 @@
+n = int(input())
+sumall = (n * (n - 1)) // 2
+
+d2 = 0
+d17 = 0
+d34 = 0
+
+for i in range(n):
+    k = int(input())
+    if k % 34 == 0:
+        d34 += 1
+    elif k % 17 == 0:
+        d17 += 1
+    elif k % 2 == 0:
+        d2 += 1
+print(sumall - ((d34 * (d34 - 1)) // 2 + d34 * (n - d34) + d2 * d17))
+
